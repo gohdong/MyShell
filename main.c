ee8fd0676b0c73d2e4a95a9da0822bbad900c39c
gohdong <aldehf420@naver.com>
2020-04-01 17:51:17 +0900

    init

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Welcome To G Shell!\n");
+    while (1)
+    {
+        printf(">>");
+        scanf("%d",&a);
+    }
+    
+
+    return 0;
+}
