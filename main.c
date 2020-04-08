1ed50152e80b6546f874d60a109907548906ab5d
gohdong <aldehf420@naver.com>
2020-04-08 22:57:20 +0900

    add cd

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
+
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(){
-    int a;
-    printf("Welcome To G Shell!\n");
+    char *inputCommand;
+    char *currentDir;
+    // printf("\033[0;31mWelcome To G Shell!\n");
+
+
     while (1)
     {
-        printf(">>");
-        scanf("%d",&a);
+        currentDir = getcwd(currentDir,200);
+        printf("gohdong@minishell %s> ",currentDir);
+        scanf("%s",inputCommand);
+        
+        if(strcmp(inputCommand,"exit")==0){
+            break;
+        }
+        
+        else if(strstr(inputCommand,"cd")!=NULL){
+            strcat(currentDir,"/..");
+            chdir(currentDir);
+        }
+        
+        else{
+            printf("Unknown Command Sorry\n");
+        }
+
+        
     }
+
+    printf("Good Bye!");
     
 
     return 0;
 }
+
